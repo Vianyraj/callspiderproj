a17d6994e23b95078676d8e3207cc3d98495b935
Vianyraj <vinay.stinson@gmail.com>
2021-03-06 12:17:19 +0100

    start
    start

--- questions/models.py
+++ questions/models.py
@@ -14,14 +14,14 @@ class Paper_model(models.Model):
     Where = models.CharField(max_length=256,null='True')
     Deadline =models.CharField(max_length=1024,null='True')
 
-# class Wiki_model(models.Model):#call for papers
-#     Title = models.CharField(max_length=1024)
-#     Dates = models.CharField(max_length=256,null='True')
-#     Url = models.CharField(max_length=1024)
-#     PaperType=models.CharField(max_length=64, null='True')
-#     Where = models.CharField(max_length=256,null='True')
-#     Deadline = models.CharField(max_length=256,null='True')
-#
+class Wiki_model(models.Model):#call for papers
+    Title = models.CharField(max_length=1024)
+    Dates = models.CharField(max_length=256,null='True')
+    Url = models.CharField(max_length=1024)
+    PaperType=models.CharField(max_length=64, null='True')
+    Where = models.CharField(max_length=256,null='True')
+    Deadline = models.CharField(max_length=256,null='True')
+
 class Bmbf_model(models.Model):
     Title = models.CharField(max_length=1024)
     Dates = models.CharField(max_length=256,null='True')
